cd5f6366191ad1617afa29afbc25432766989b91
firasatmunir <zapp3r1997@gmail.com>
2022-07-04 09:23:17 +0500

    updated task4

---
+++ task4.js
@@ -0,0 +1,5 @@
+var cities=["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+document.write("Cities list: <br>"+cities)
+
+var selectedCities=cities.slice(1,3)
+document.write("<br><br>Selected cities list: <br>"+selectedCities)
